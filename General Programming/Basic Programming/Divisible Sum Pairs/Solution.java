72adcf894eb6259fac4619902543ecb4bdcfe7e8
Rodney Shaghoulian <9rodney@gmail.com>
2019-03-17 10:37:08 -0700

    Updated 1 solution

--- General Programming/Basic Programming/Divisible Sum Pairs/Solution.java
+++ General Programming/Basic Programming/Divisible Sum Pairs/Solution.java
@@ -29,23 +29,55 @@ this complement bucket can be paired with our original number to create a sum di
 
 */
 
-import java.util.Scanner;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int k = scan.nextInt();
 
+    static int divisibleSumPairs(int n, int k, int[] ar) {
         int [] bucket = new int[k];
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            int modValue = scan.nextInt() % k;
+        for (int value : ar) {
+            int modValue = value % k;
             count += bucket[(k - modValue) % k]; // adds # of elements in complement bucket
             bucket[modValue]++;                  // saves in bucket
         }
-       
-        scan.close();
-        System.out.println(count);
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] ar = new int[n];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = divisibleSumPairs(n, k, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
     }
 }
