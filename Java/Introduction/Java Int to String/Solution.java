344d1ae8def389f0ba4cf4d9a631422cbf3c14d5
Rodney Shaghoulian <9rodney@gmail.com>
2019-03-28 10:11:34 -0700

    Remove unnecessary boilerplate code.

--- Java/Introduction/Java Int to String/Solution.java
+++ Java/Introduction/Java Int to String/Solution.java
@@ -1,47 +1,7 @@
 // Author: Rodney Shaghoulian
 // Github: github.com/RodneyShag
 
-import java.util.*;
-import java.security.*;
-
-public class Solution {
-    public static void main(String[] args) {
-        DoNotTerminate.forbidExit();
-        try {
-            Scanner in = new Scanner(System.in);
-            int n = in .nextInt();
-            in.close();
-             
-            /* Here are 3 ways to convert an int to a String */
-            // String s = String.valueOf(n);
-            // String s = Integer.toString(n);
-            String s = "" + n;
-
-            if (n == Integer.parseInt(s)) {
-              System.out.println("Good job");
-            } else {
-              System.out.println("Wrong answer.");
-            }
-        } catch (DoNotTerminate.ExitTrappedException e) {
-              System.out.println("Unsuccessful Termination!!");
-        }
-    }
-}
-
-// The following class will prevent you from terminating the code using exit(0)!
-class DoNotTerminate {
-    public static class ExitTrappedException extends SecurityException {
-        private static final long serialVersionUID = 1;
-    }
-    public static void forbidExit() {
-        final SecurityManager securityManager = new SecurityManager() {
-            @Override
-            public void checkPermission(Permission permission) {
-            if (permission.getName().contains("exitVM")) {
-                throw new ExitTrappedException();
-            }
-        }
-    };
-    System.setSecurityManager(securityManager);
-    }
-}
+/* Here are 3 ways to convert an int to a String. Use any of the 3 methods. */
+// String s = String.valueOf(n);
+// String s = Integer.toString(n);
+String s = "" + n;
