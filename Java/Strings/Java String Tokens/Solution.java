fb078617bf7af0f08524c99107ea796877a9e239
Rodney Shaghoulian <9rodney@gmail.com>
2019-03-28 17:29:11 -0700

    Updated 1 solution

--- Java/Strings/Java String Tokens/Solution.java
+++ Java/Strings/Java String Tokens/Solution.java
@@ -10,7 +10,7 @@ public class Solution {
         String s = scan.nextLine();
         scan.close();
         
-        s = s.trim(); // so that .split() works properly
+        s = removeLeadingNonLetters(s);
         
         /* Check special cases */
         if (s.length() == 0) {
@@ -19,12 +19,22 @@ public class Solution {
         }
         
         /* Split on all non-alphabetic characters */
-        String [] words = s.split("[^a-zA-Z]+");
+        String[] words = s.split("[^a-zA-Z]+");
         
         /* Print output */
         System.out.println(words.length);
         for (String word : words) {
             System.out.println(word);
         }
+    }
+
+    private static String removeLeadingNonLetters(String str) {
+        int i;
+        for (i = 0; i < str.length(); i++) {
+            if (Character.isLetter(str.charAt(i))) {
+                break;
+            }
+        }
+        return str.substring(i);
     }
 }
