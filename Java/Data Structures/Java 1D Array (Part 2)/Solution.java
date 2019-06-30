f2b9e3999da29bf8ede5de437a5735543fadfd41
Rodney Shaghoulian <9rodney@gmail.com>
2019-06-30 10:37:39 -0700

    Updated 1 solution

--- Java/Data Structures/Java 1D Array (Part 2)/Solution.java
+++ Java/Data Structures/Java 1D Array (Part 2)/Solution.java
@@ -2,20 +2,23 @@
 // Github: github.com/RodneyShag
 
 public static boolean canWin(int leap, int[] game) {
+	if (game == null) {
+		return false;
+	}
     return isSolvable(leap, game, 0);
 }
 
 private static boolean isSolvable(int leap, int[] game, int i) {
-    /* Base Cases */
-    if (i < 0 || game[i] == 1) {
-        return false;
-    } else if (i + 1 >= game.length || i + leap >= game.length) {
+    // Base Cases
+    if (i >= game.length) {
         return true;
+    } else if (i < 0 || game[i] == 1) {
+        return false;
     }
     
     game[i] = 1; // marks as visited
 
-    /* Recursive Cases (Tries +m first to try to finish game quickly) */
+    // Recursive Cases (Tries +m first to try to finish game quickly)
     return isSolvable(leap, game, i + leap)
         || isSolvable(leap, game, i + 1)
         || isSolvable(leap, game, i - 1);
