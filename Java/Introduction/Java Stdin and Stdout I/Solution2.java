0ae3719ce5a0c40a7043c9144409048e147451da
Rahul Awari <47710382+awarirahul365@users.noreply.github.com>
2019-10-06 18:58:13 +0530

    Create Solution2.java

---
+++ Java/Introduction/Java Stdin and Stdout I/Solution2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Hacker1 {
+
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        int num3=sc.nextInt();
+        System.out.println(num1+"\n"+num2+"\n"+num3);
+        
+    }
+
+}
