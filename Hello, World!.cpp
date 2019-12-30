3ccde3d6533bdb906ee5f9054bbaf4f586316ad7
Sanskriti Chauhan <skritichauhan@gmail.com>
2019-12-31 04:10:58 +0530

    Add files via upload

---
+++ Hello, World!.cpp
@@ -0,0 +1,17 @@
+
+
+/**
+*   A line of code that prints "Hello, World!" on a new line is provided in the editor. 
+*   Write a second line of code that prints the contents of 'parameterVariable' on a new line.
+*
+*	Parameter:
+*   parameterVariable - A string of text.
+**/
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+    console.log(parameterVariable);
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+    
+}
+
