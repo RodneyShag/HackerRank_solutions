74de9902f4a7a161ae5d94957884fae1bd4953e7
Huy Tran <quanghuy2621998@gmail.com>
2020-02-25 14:15:59 +0700

    Your Solution can't pass all test case, This is a good sulotion, i hope useful
    Thanks very much

--- Java/Data Structures/Java 2D Array/Solution.java
+++ Java/Data Structures/Java 2D Array/Solution.java
@@ -1,36 +1,36 @@
 // github.com/RodneyShag
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Solution {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int arr[][] = new int[6][6];
-        for (int row = 0; row < 6; row++) {
-            for (int col = 0; col < 6; col++) {
-                arr[row][col] = scan.nextInt();
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        int a[][] = new int[6][6];
+        int maxSum = Integer.MIN_VALUE;
+        try (Scanner scanner = new Scanner(System.in);)
+        {
+            for(int i = 0; i < 6; i++)
+            {
+                for(int j = 0; j < 6; j++)
+                {
+                    a[i][j] = scanner.nextInt();
+                    if (i > 1 && j > 1)
+                    {
+                        int sum =
+                            a[i][j]
+                            + a[i][j-1]
+                            + a[i][j-2]
+                            + a[i-1][j-1]
+                            + a[i-2][j]
+                            + a[i-2][j-1]
+                            + a[i-2][j-2];
+                        if (sum > maxSum) {maxSum = sum;}
+                    }
+                }
             }
         }
-        scan.close();
-        
-        System.out.println(maxHourglass(arr));
-    }
-    
-    public static int maxHourglass(int [][] arr) {
-        int max = Integer.MIN_VALUE;
-        for (int row = 0; row < 4; row++) {
-            for (int col = 0; col < 4; col++) {
-                int sum = findSum(arr, row, col);
-                max = Math.max(max, sum);
-            }
-        }
-        return max;
-    }
-    
-    private static int findSum(int [][] arr, int r, int c) {
-        int sum = arr[r+0][c+0] + arr[r+0][c+1] + arr[r+0][c+2] 
-                                + arr[r+1][c+1] + 
-                  arr[r+2][c+0] + arr[r+2][c+1] + arr[r+2][c+2];
-        return sum;
+        System.out.println(maxSum);
     }
 }
