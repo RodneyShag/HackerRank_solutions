b0286c9761bd5c71f425999a3631b34e6a300f0e
Sushrut Shukla <sushrutshukla29@gmail.com>
2020-10-01 09:37:33 +0530

    Update Solution.java

--- Java/Introduction/Java Loops I/Solution.java
+++ Java/Introduction/Java Loops I/Solution.java
@@ -1,12 +1,13 @@
 // github.com/RodneyShag
 
 import java.util.Scanner;
+import java.io.*;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int multiplier = scan.nextInt();
-        scan.close();
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int multiplier = Integer.parseInt(br.readLine());
+       
         for (int i = 1; i <= 10; i++) {
             System.out.format("%d x %d = %d%n", multiplier, i , i * multiplier);
         }
